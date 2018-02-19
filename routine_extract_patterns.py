41fd54b2ef4721d0283a100bb2fc64de9037bcc3
JonKoala <victor.santana.lemos@gmail.com>
2018-02-19 14:47:24 -0300

    agrupa rotinas de extracao de padroes em regex
    tambem atualiza regex para valor monetario

--- routine_extract_patterns.py
+++ routine_extract_patterns.py
@@ -2,17 +2,37 @@ import inout
 from db import Dbinterface
 from db.models import Publicacao, Predicao, Patterns
 
+import argparse
 import re
 from decimal import Decimal
 
 
 ##
+# Command line arguments
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--full', help='Extract patterns for the whole database', action='store_true')
+
+reset_base = parser.parse_args().full
+
+
+##
 # Utils
 
-def to_decimal(match):
-    return Decimal(''.join(match.group(0).split()).replace('$', '').replace('.', '').replace(',', '.'))
+# regex patterns
+re_money = re.compile('\$\s*[1-9][\d\.,]*\d')
+re_fractional_part = re.compile('[\.,]\d{1,2}$')
 
-re_money = re.compile('\$\s*[1-9][\d\.]*(,\d{2})?')
+def to_decimal(match):
+    treated = ''.join(match.group(0).split()).replace('$', '')
+
+    integer_match = re.split(re_fractional_part, treated)
+    integer_part = re.sub('[\.,]', '', integer_match[0])
+
+    fractional_match = re.search(re_fractional_part, treated)
+    fractional_part = re.sub('[\.,]', '', fractional_match.group(0)) if fractional_match else '00'
+
+    return Decimal('{}.{}'.format(integer_part, fractional_part))
 
 
 ##
@@ -21,10 +41,13 @@ re_money = re.compile('\$\s*[1-9][\d\.]*(,\d{2})?')
 appconfig = inout.read_yaml('./appconfig')
 dbi = Dbinterface(appconfig['db']['connectionstring'])
 
-# get unextracted publicacoes
+# get publicacoes
 with dbi.opensession() as session:
-    already_extracted = session.query(Patterns.publicacao_id)
-    to_extract = session.query(Predicao).join(Predicao.publicacao).filter(Predicao.publicacao_id.notin_(already_extracted))
+    to_extract = session.query(Predicao).join(Predicao.publicacao)
+    if reset_base:
+        already_extracted = session.query(Patterns.publicacao_id)
+        to_extract = to_extract.filter(Predicao.publicacao_id.notin_(already_extracted))
+
     data = [(predicao.publicacao.id, predicao.publicacao.corpo) for predicao in to_extract]
 
 
@@ -49,6 +72,11 @@ for index, publicacao in enumerate(data):
 # Persist patterns on database
 
 with dbi.opensession() as session:
+
+    # clean old entries
+    if reset_base:
+        session.query(Patterns).delete()
+        session.flush()
 
     # insert new patterns
     for result in results:
