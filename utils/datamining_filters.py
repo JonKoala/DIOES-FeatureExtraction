e6959338a6a5404e2cfbfa56f46b588f5786f137
JonKoala <victor.santana.lemos@gmail.com>
2019-10-04 19:19:44 -0300

    adiciona tipos de publicacao do diario da amunes

--- utils/datamining_filters.py
+++ utils/datamining_filters.py
@@ -2,4 +2,6 @@
 # -*- coding: utf-8 -*-
 
 classe_filters = ['SAÚDE E ASSISTÊNCIA SOCIAL', 'EDUCAÇÃO E SEGURANÇA PÚBLICA', 'ENGENHARIA E MEIO AMBIENTE', 'TECNOLOGIA DA INFORMAÇÃO', 'GERAL', 'PREVIDÊNCIA E PESSOAL']
-publicacao_tipo_filters = ['Aviso de Licitação', 'Contrato', 'Aditivo', 'Ata Registro de Preço', 'Inexigibilidade de Licitação', 'Dispensa de Licitação', 'Portaria', 'Edital', 'Convocação']
+publicacao_tipo_filters_ioes = ['Aviso de Licitação', 'Contrato', 'Aditivo', 'Ata Registro de Preço', 'Inexigibilidade de Licitação', 'Dispensa de Licitação', 'Portaria', 'Edital', 'Convocação']
+publicacao_tipo_filters_amunes = ['Ata de registro de preços', 'Atas', 'Concursos', 'Contratos', 'Convênios', 'Decretos', 'Editais de Notificação', 'Leis', 'Licitações', 'Outras publicações', 'Portarias', 'Resoluções']
+publicacao_tipo_filters = publicacao_tipo_filters_ioes + publicacao_tipo_filters_amunes
